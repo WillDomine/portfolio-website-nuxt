fa32c502bb50b85db5e4503049307bafb3827ac5
Will Domine <willdomine1@outlook.com>
2026-01-27 21:12:57 -0600

    navbar basics still working on design added i18n for jp to en

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,36 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
-
-  modules: ['shadcn-nuxt'],
+  i18n: {
+    langDir: 'locales',
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+        file: 'en.json',
+        name: 'English'
+      },
+      {
+        code: 'jp',
+        iso: 'ja-JP',
+        file: 'jp.json',
+        name: '日本語'
+      }
+    ],
+    defaultLocale: 'en',
+    strategy: 'prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+      alwaysRedirect: true,
+      fallbackLocale: 'en',
+    },
+  },
+  modules: ['shadcn-nuxt', '@nuxtjs/i18n',],
 })
