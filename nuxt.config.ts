7750e78fef7f831bcc2d3ec8fc277e0b3b09d1ec
Will Domine <willdomine1@outlook.com>
2026-01-28 09:13:00 -0600

    title added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app : {
+    head : {
+      title: 'Portfolio'
+    }
+  },
   css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [
