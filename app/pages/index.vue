ea25971dfff8c6109a30050d3755109dc583bcea
Will Domine <willdomine1@outlook.com>
2026-02-03 14:53:54 -0600

    Revert "snapping"
    This reverts commit e3d164049cc588810010a6335839ec4a0b095316.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-    <div class="h-screen w-full snap-y snap-mandatory overflow-y-scroll scroll-smooth">
+    <div class="min-h-screen scroll-smooth">
         <SnapContainer id="home"><HomeSection/></SnapContainer>
         <SnapContainer id="about"><AboutSection /></SnapContainer>
         <SnapContainer id="projects"><ProjectsSection/></SnapContainer>
