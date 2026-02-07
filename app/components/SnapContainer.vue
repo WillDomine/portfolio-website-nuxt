ff255cf159e7666eedf7c558159651fc56e9a168
Will Domine <willdomine1@outlook.com>
2026-02-07 15:49:38 -0600

    starting scroll snap clean up.

--- app/components/SnapContainer.vue
+++ app/components/SnapContainer.vue
@@ -4,7 +4,7 @@ defineProps({
 })
 </script>
 <template>
-    <section :id="id" class="relative min-h-dvh w-full md:snap-start flex flex-col justify-center py-4 md:py-0 overflow-hidden">
+    <section :id="id" class="relative min-h-dvh w-full flex flex-col justify-center py-4 md:py-0 overflow-hidden">
         <slot />
     </section>
 </template>
